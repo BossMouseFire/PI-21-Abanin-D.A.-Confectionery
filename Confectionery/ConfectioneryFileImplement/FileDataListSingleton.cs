8172d62915fd76089311113b5cf0253a265d74ed
Данил Абанин <abanin-02@mail.ru>
2022-02-13 23:39:43 +0300

    lab2

---
+++ Confectionery/ConfectioneryFileImplement/FileDataListSingleton.cs
@@ -0,0 +1,133 @@
+﻿using ConfectioneryContracts.Enums;
+using ConfectioneryFileImplement.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ConfectioneryFileImplement
+{
+    public class FileDataListSingleton
+    {
+        private static FileDataListSingleton instance;
+        private readonly string ComponentFileName = "Component.xml";
+        private readonly string OrderFileName = "Order.xml";
+        private readonly string PastryFileName = "Pastry.xml";
+        public List<Component> Components { get; set; }
+        public List<Order> Orders { get; set; }
+        public List<Pastry> Products { get; set; }
+        private FileDataListSingleton()
+        {
+            Components = LoadComponents();
+            Orders = LoadOrders();
+            Products = LoadProducts();
+        }
+        public static FileDataListSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new FileDataListSingleton();
+            }
+            return instance;
+        }
+        ~FileDataListSingleton()
+        {
+            SaveComponents();
+            SaveOrders();
+            SaveProducts();
+        }
+        private List<Component> LoadComponents()
+        {
+            var list = new List<Component>();
+            if (File.Exists(ComponentFileName))
+            {
+                var xDocument = XDocument.Load(ComponentFileName);
+                var xElements = xDocument.Root.Elements("Component").ToList();
+            foreach (var elem in xElements)
+                {
+                    list.Add(new Component
+                    {
+                        Id = Convert.ToInt32(elem.Attribute("Id").Value),
+                        ComponentName = elem.Element("ComponentName").Value
+                    });
+                }
+            }
+            return list;
+        }
+        private List<Order> LoadOrders()
+        {
+            // прописать логику
+        }
+        private List<Pastry> LoadProducts()
+        {
+            var list = new List<Pastry>();
+            if (File.Exists(PastryFileName))
+            {
+                var xDocument = XDocument.Load(PastryFileName);
+                var xElements = xDocument.Root.Elements("Product").ToList();
+                foreach (var elem in xElements)
+                {
+                    var prodComp = new Dictionary<int, int>();
+                    foreach (var component in
+                   elem.Element("ProductComponents").Elements("ProductComponent").ToList())
+                    {
+                        prodComp.Add(Convert.ToInt32(component.Element("Key").Value),
+                       Convert.ToInt32(component.Element("Value").Value));
+                    }
+                    list.Add(new Pastry
+                    {
+                        Id = Convert.ToInt32(elem.Attribute("Id").Value),
+                        PastryName = elem.Element("ProductName").Value,
+                        Price = Convert.ToDecimal(elem.Element("Price").Value),
+                        PastryComponents = prodComp
+                    });
+                }
+            }
+            return list;
+        }
+        private void SaveComponents()
+        {
+            if (Components != null)
+            {
+                var xElement = new XElement("Components");
+                foreach (var component in Components)
+                {
+                    xElement.Add(new XElement("Component",
+                    new XAttribute("Id", component.Id),
+                    new XElement("ComponentName", component.ComponentName)));
+                }
+                var xDocument = new XDocument(xElement);
+            xDocument.Save(ComponentFileName);
+            }
+        }
+        private void SaveOrders()
+        {
+            // прописать логику
+        }
+        private void SaveProducts()
+        {
+            if (Products != null)
+            {
+                var xElement = new XElement("Products");
+                foreach (var product in Products)
+                {
+                    var compElement = new XElement("ProductComponents");
+                    foreach (var component in product.PastryComponents)
+                    {
+                        compElement.Add(new XElement("ProductComponent",
+                        new XElement("Key", component.Key),
+                        new XElement("Value", component.Value)));
+                    }
+                    xElement.Add(new XElement("Product",
+                     new XAttribute("Id", product.Id),
+                     new XElement("ProductName", product.PastryName),
+                     new XElement("Price", product.Price),
+                     compElement));
+                }
+                var xDocument = new XDocument(xElement);
+                xDocument.Save(PastryFileName);
+            }
+        }
+    }
+}
---
+++ Confectionery/ConfectioneryFileImplement/Implements/PastryStorage.cs
@@ -0,0 +1,119 @@
+﻿using ConfectioneryContracts.BindingModels;
+using ConfectioneryContracts.StoragesContracts;
+using ConfectioneryContracts.ViewModels;
+using ConfectioneryFileImplement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ConfectioneryFileImplement.Implements
+{
+    public class PastryStorage : IPastryStorage
+    {
+        private readonly FileDataListSingleton source;
+        public PastryStorage()
+        {
+            source = FileDataListSingleton.GetInstance();
+        }
+        public List<PastryViewModel> GetFullList()
+        {
+            return source.Products
+            .Select(CreateModel)
+            .ToList();
+        }
+        public List<PastryViewModel> GetFilteredList(PastryBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            return source.Products
+            .Where(rec => rec.PastryName.Contains(model.PastryName))
+            .Select(CreateModel)
+            .ToList();
+        }
+        public PastryViewModel GetElement(PastryBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            var pastry = source.Products
+            .FirstOrDefault(rec => rec.PastryName == model.PastryName || rec.Id
+           == model.Id);
+            return pastry != null ? CreateModel(pastry) : null;
+        }
+        public void Insert(PastryBindingModel model)
+        {
+        int maxId = source.Products.Count > 0 ? source.Components.Max(rec => rec.Id)
+: 0;
+            var element = new Pastry
+            {
+                Id = maxId + 1,
+                PastryComponents = new
+           Dictionary<int, int>()
+            };
+            source.Products.Add(CreateModel(model, element));
+        }
+        public void Update(PastryBindingModel model)
+        {
+            var element = source.Products.FirstOrDefault(rec => rec.Id == model.Id);
+            if (element == null)
+            {
+                throw new Exception("Элемент не найден");
+            }
+            CreateModel(model, element);
+        }
+        public void Delete(PastryBindingModel model)
+        {
+            Pastry element = source.Products.FirstOrDefault(rec => rec.Id == model.Id);
+            if (element != null)
+            {
+                source.Products.Remove(element);
+            }
+            else
+            {
+                throw new Exception("Элемент не найден");
+            }
+        }
+        private static Pastry CreateModel(PastryBindingModel model, Pastry pastry)
+        {
+            pastry.PastryName = model.PastryName;
+            pastry.Price = model.Price;
+            // удаляем убранные
+            foreach (var key in pastry.PastryComponents.Keys.ToList())
+            {
+                if (!model.PastryComponents.ContainsKey(key))
+                {
+                    pastry.PastryComponents.Remove(key);
+                }
+            }
+            // обновляем существуюущие и добавляем новые
+            foreach (var component in model.PastryComponents)
+            {
+                if (pastry.PastryComponents.ContainsKey(component.Key))
+                {
+                    pastry.PastryComponents[component.Key] =
+                   model.PastryComponents[component.Key].Item2;
+                }
+                else
+                {
+                    pastry.PastryComponents.Add(component.Key,
+                   model.PastryComponents[component.Key].Item2);
+                }
+            }
+            return pastry;
+        }
+        private PastryViewModel CreateModel(Pastry pastry)
+        {
+            return new PastryViewModel
+            {
+                Id = pastry.Id,
+                PastryName = pastry.PastryName,
+                Price = pastry.Price,
+                PastryComponents = pastry.PastryComponents.ToDictionary(recPC => recPC.Key, recPC =>
+                (source.Components.FirstOrDefault(recC => recC.Id ==
+                recPC.Key)?.ComponentName, recPC.Value))
+            };
+        }
+    }
+}
