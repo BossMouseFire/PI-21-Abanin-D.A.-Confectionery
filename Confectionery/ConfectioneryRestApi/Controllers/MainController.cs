a4cc63d672fa9edc3746ada9c8f9f5afd960eaf1
Данил Абанин <abanin-02@mail.ru>
2022-03-28 22:01:46 +0300

    lab5

---
+++ Confectionery/ConfectioneryRestApi/Controllers/MainController.cs
@@ -0,0 +1,34 @@
+﻿using ConfectioneryContracts.BindingModels;
+using ConfectioneryContracts.BusinessLogicsContracts;
+using ConfectioneryContracts.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConfectioneryRestApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        private readonly IOrderLogic _order;
+        private readonly IPastryLogic _pastry;
+        public MainController(IOrderLogic order, IPastryLogic pastry)
+        {
+            _order = order;
+            _pastry = pastry;
+        }
+        [HttpGet]
+        public List<PastryViewModel> GetProductList() => _pastry.Read(null)?.ToList();
+        [HttpGet]
+        public PastryViewModel GetProduct(int productId) => _pastry.Read(new PastryBindingModel
+        { Id = productId })?[0];
+        [HttpGet]
+        public List<OrderViewModel> GetOrders(int clientId) => _order.Read(new
+       OrderBindingModel
+        { ClientId = clientId });
+        [HttpPost]
+        public void CreateOrder(CreateOrderBindingModel model) =>
+       _order.CreateOrder(model);
+    }
+}
---
+++ Confectionery/ConfectioneryRestApi/Startup.cs
@@ -0,0 +1,59 @@
+using ConfectioneryBusinessLogic.BusinessLogics;
+using ConfectioneryContracts.BusinessLogicsContracts;
+using ConfectioneryContracts.StoragesContracts;
+using ConfectioneryDatabaseImplement.Implements;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace ConfectioneryRestApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IClientStorage, ClientStorage>();
+            services.AddTransient<IOrderStorage, OrderStorage>();
+            services.AddTransient<IPastryStorage, PastryStorage>();
+            services.AddTransient<IOrderLogic, OrderLogic>();
+            services.AddTransient<IClientLogic, ClientLogic>();
+            services.AddTransient<IPastryLogic, PastryLogic>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title =
+                "AbstractShopRestApi",
+                    Version = "v1"
+                });
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ConfectioneryRestApi v1"));
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+
+}
