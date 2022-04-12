b0041c5fe4640145bc84547d8ad6131313a653ac
Данил Абанин <abanin-02@mail.ru>
2022-04-12 08:34:28 +0300

    fixed Lab5

--- Confectionery/ConfectioneryView/FormMain.cs
+++ Confectionery/ConfectioneryView/FormMain.cs
@@ -31,6 +31,7 @@ namespace ConfectioneryView
                     dataGridView.DataSource = list;
                     dataGridView.Columns[0].Visible = false;
                     dataGridView.Columns[1].Visible = false;
+                    dataGridView.Columns[3].Visible = false;
                 }
             }
             catch (Exception ex)
