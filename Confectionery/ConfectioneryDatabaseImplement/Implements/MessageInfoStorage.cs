f90083bb6a9cc2d1a123fe27fb24987e4cf672ac
Данил Абанин <abanin-02@mail.ru>
2022-04-30 15:15:20 +0300

    completed Lab7

--- Confectionery/ConfectioneryDatabaseImplement/Implements/MessageInfoStorage.cs
+++ Confectionery/ConfectioneryDatabaseImplement/Implements/MessageInfoStorage.cs
@@ -58,7 +58,7 @@ namespace ConfectioneryDatabaseImplement.Implements
             context.MessageInfoes.Add(new MessageInfo
             {
                 MessageId = model.MessageId,
-                ClientId = model.ClientId,
+                ClientId = model.ClientId != null ? model.ClientId : context.Clients.FirstOrDefault(rec => rec.Login == model.FromMailAddress).Id,
                 SenderName = model.FromMailAddress,
                 DateDelivery = model.DateDelivery,
                 Subject = model.Subject,
